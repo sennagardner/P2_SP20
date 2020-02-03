6b1ec99db2a3fc0e9086445c6dd4680b61085026
sennagardner <sgardner@fwparker.org>
2020-02-03 08:26:27 -0600

    Added import notes

--- Problems/02_lists.py
+++ Problems/02_lists.py
@@ -4,38 +4,75 @@
 # PROBLEM 1 (Using List Comprehensions - 8pts)
 # Use list comprehensions to do the following:
 # a) Make a list of numbers from 1 to 100
+my_list = [x for x in range(101)]
+print(my_list)
 # b) Make a list of even numbers from 20 to 40
+my_list2 = [x for x in range(20, 40) if x % 2 == 0]
+print(my_list2)
 # c) Make a list of squares from 1 to 100 (1 ** 2 to 100 ** 2)
+my_list3 = [x ** 2 for x in range(101)]
+print(my_list3)
 # d) Make a list of all positive numbers in my_list below.
 my_list = [-77, -78, 82, 81, -40, 2, 62, 65, 74, 48, -37, -52, 90, -84, -79, -45, 47, 60, 35, -18]
+my_positive = [x for x in my_list if x > 0]
+print(my_positive)
 
 # PROBLEM 2 (Import the number list - 3pts)
 # The Problems directory contains a file called "number_list.py"
 # import this file which contains num_list
 # Print the last 5 numbers in num_list
 
+from Problems.number_list import *
+
+print(num_list[-5:])
 
 # PROBLEM 3 (List functions and methods - 8pts)
 # Find and print the highest number in num_list (1pt)
+print(max(num_list))
 # Find and print the lowest number in num_list (1pt)
+print(min(num_list))
 # Find and print the average of num_list (2pts)
+print(sum(num_list) / len(num_list))
 # Remove the lowest number from num_list (2pt)
+num_list.sort()
+num_list.pop(0)
+print(num_list)
 # Create and print a new list called top_ten which contains only the 10 highest numbers in num_list(2pts)
-
+top_ten = num_list[-10:]
+print(top_ten)
 
 # PROBLEM 4 (4pts)
 # Find the number which appears most often in num_list?
 
+from statistics import mode
+
+def most_common(num_list):
+    return (mode(num_list))
+
+print(most_common(num_list))
 
 # CHALLENGE PROBLEMS (2pts)
 # TOUGH PROBLEMS, BUT FEW POINTS
 
 # Find the number of prime numbers in num_list?
 # Hint: One way is to just start removing the ones that aren't
+for num in num_list:
+    if num % 2 == 0:
+        num_list.remove(num)
+for num in num_list:
+    if num % 3 == 0:
+        num_list.remove(num)
+for num in num_list:
+    if num % 4 == 0:
+        num_list.remove(num)
+for num in num_list:
+    if num % 5 == 0:
+        num_list.remove(num)
+for num in num_list:
+    if num % 7 == 0:
+        num_list.remove(num)
+print(num_list)
 
 # Find the number of palindromes
 # Hint: This may be easier to do with strings
-
-
-
 
