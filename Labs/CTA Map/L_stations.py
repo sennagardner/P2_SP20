03bbe02b4223dcee1a4fbc88e25f1d430716b29a
sennagardner <sgardner@fwparker.org>
2020-04-29 14:11:51 -0500

    L stations lab

--- Labs/CTA Map/L_stations.py
+++ Labs/CTA Map/L_stations.py
@@ -10,10 +10,53 @@
 
 # Tricky parts of this one
 ## The location is in tuple format.  If you have trouble converting it, try this:
-my_string = '(41.2, -87.9)'
-my_tuple = eval(my_string)
-print(my_tuple)
-print(type(my_tuple))
+import folium
+import csv
+from folium import plugins
+
+
+with open('CTA_-_System_Information_-_List_of__L__Stops (1).csv') as f:
+    reader = csv.reader(f)
+    data = list(reader)
+
+data.pop(0)
+
+
+train_stopslist = [eval(x[-1]) for x in data]
+stop_names = [x[3] for x in data]
+color_list = []
+
+for train in data:
+    if train[7] == 'true':
+        color_list.append('red')
+    elif train[8] == 'true':
+        color_list.append('blue')
+    elif train[9] == 'true':
+        color_list.append('green')
+    elif train[10] == 'true':
+        color_list.append('    #A52A2A')
+    elif train[11] == 'true' or train[12] == 'true':
+        color_list.append('purple')
+    elif train[13] == 'true':
+        color_list.append('#FFFF00')
+    elif train[14] == 'true':
+        color_list.append('pink')
+    elif train[-2] == 'true':
+        color_list.append('orange')
+    else:
+        color_list.append('black')
+
+
+train_map = folium.Map(location=[41.880443, -87.644107], zoom_start=11)
+
+for i in range(len(data)):
+    folium.Marker(location=(train_stopslist[i]),
+                  popup=stop_names[i],
+                  icon=folium.plugins.BeautifyIcon(border_color=(color_list[i]), icon='train', prefix='fa')
+                  ).add_to(train_map)
+
+
+train_map.save('train_map.html')
 
 
 # If you have extra time, try to put some html into the popup.
--- Labs/PrideAndPrejudice/PridePrejudice.py
+++ Labs/PrideAndPrejudice/PridePrejudice.py
@@ -21,6 +21,19 @@ A common Python pattern to count objects, produce histograms, or update stats is
 # Create Bar Graph (5pts)
 # Include descriptive titles and labels (5pts)
 
+import requests
+import matplotlib.pyplot as plt
+
+url = "https://www.gutenberg.org/files/1342/1342-h/1342-h.htm"
+pride_prejudice = requests.get(url).text
+
+print(pride_prejudice[:1000])
+
+wordlist = pride_prejudice.split()
+wordlist= [x.upper().strip('?.:;!\\<>{}\n\t') for x in wordlist]
+
+print(wordlist[:1000])
+
 
 # CHALLENGE (OPTIONAL)
 # Here is a list of the 1000 most common words in English: https://gist.githubusercontent.com/deekayen/4148741/raw/98d35708fa344717d8eee15d11987de6c8e26d7d/1-1000.txt
